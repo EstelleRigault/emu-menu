2d0657b9e57663f0616b6678d6a8615928236888
Alexander Handley <alexh@reqfire.com>
2016-11-15 09:36:13 +1100

    cloned tutorial react app and set up for firebase

---
+++ Final Codebase/src/base.js
@@ -0,0 +1,9 @@
+import Rebase from 're-base';
+
+const base = Rebase.createClass({
+  apiKey: "AIzaSyB71R554GT-YqOzofSaB-Q7JX8AdlzX9Ls",
+  authDomain: "emu-menu-c355a.firebaseapp.com",
+  databaseURL: "https://emu-menu-c355a.firebaseio.com",
+});
+
+export default base;
---
+++ Final Codebase/src/recepie-list.js
@@ -0,0 +1,56 @@
+// This is just some sample data so you don't have to think of your own!
+const recipes = {
+  recipe1: {
+    name: 'Turkey Chilli Jacket Potatoes',
+    image: 'http://www.bbcgoodfood.com/sites/default/files/styles/teaser_item/public/recipe_images/recipe-image-legacy-id--1071476_11.jpg?itok=8PDpQcg4',
+    desc: 'Turkey mince is cheap and lean - flavour Mexican-style with cumin and paprika and serve in crisp baked potatoes.',
+    price: 8,
+    status: 'available',
+    ingredients: {
+      ingredient1: "4 potatoes",
+      ingredient2: "1 large turkey",
+      ingredient3: "10 grams Chilli"
+    }
+  },
+
+  recipe2: {
+    name: 'Entertaining ya mates',
+    image: 'http://i.onionstatic.com/onion/1404/4/16x9/565.jpg',
+    desc: '\"Yeah nah come round any time boys. I\'m just kicking back you know yeah righto, sorry mate see you soon\"',
+    price: 8,
+    status: 'available',
+    ingredients: {
+      ingredient1: "2 slabs of mate makers(VBs)",
+      ingredient2: "1 deck of Winnie blues",
+      ingredient3: "20 grams Chilli"
+    }
+  },
+
+  recipe3: {
+    name: 'Home by yourself pasta',
+    image: 'http://healthyrecipesblogs.com/wp-content/uploads/2012/08/olive-oil-pasta2.jpg',
+    desc: 'Pasta for when no one responded to your texts. That\'s right texts, multiple messages to the people who were meant to be your friends but just so happen to have started living like your only purpose is to be there for their whims.',
+    price: 8,
+    status: 'available',
+    ingredients: {
+      ingredient1: "1 pasta packet",
+      ingredient2: "Some olive oil",
+      ingredient3: "TV I guess"
+    }
+  },
+
+  recipe4: {
+    name: 'Late night snack',
+    image: 'http://healthyrecipesblogs.com/wp-content/uploads/2012/08/olive-oil-pasta2.jpg',
+    desc: 'Your dinner sucked, pasta and olive oil, really? Maybe the reason no one replied is because you expect to much from your friends. I mean how do you not know that they had \'Home by yourself pasta\' as well. Go on eat the caramel tim-tam brownie tonight but tomorrow is a new day.',
+    price: 8,
+    status: 'available',
+    ingredients: {
+      ingredient1: "1 packet of tim-tams",
+      ingredient2: "Left-over brownie",
+      ingredient3: "Some olive oil"
+    }
+  },
+};
+
+export default recipes;
